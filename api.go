10e90714c3b80c238f0bb61871201acf94a74da6
Holden Offmenn <holden.luz@grupoicts.org.br>
2023-06-02 11:55:55 -0400

    Add api to do a interface with client

---
+++ api.go
@@ -0,0 +1,13 @@
+package functions
+
+type Client interface{
+
+	MapsCheck(map1, map2 map[string]string) bool
+
+	CompareMaps(map1, map2 interface{}) bool 
+
+	CopyMapStringString(m map[string]string) map[string]string
+
+	CopyMapStringInt(m map[string]int64) map[string]int64
+
+}
