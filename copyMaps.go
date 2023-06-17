d7cddd90af4407c6099ad6b1ebe5963a4b8cf36f
Holden Offmenn <holden.luz@grupoicts.org.br>
2023-06-17 10:45:53 -0400

    Add two copy maps

--- copyMaps.go
+++ copyMaps.go
@@ -5,21 +5,21 @@ import (
 )
 
 //MakeMapCopy will received a map
-// func CopyMapStringString(m map[string]string) map[string]string {
-// 	copy := make(map[string]string)
-// 	for key, value := range m {
-// 		copy[key] = value
-// 	}
-// 	return copy
-// }
+func CopyMapStringString(m map[string]string) map[string]string {
+	copy := make(map[string]string)
+	for key, value := range m {
+		copy[key] = value
+	}
+	return copy
+}
 
-// func CopyMapStringInt(m map[string]int64) map[string]int64 {
-// 	copy := make(map[string]int64)
-// 	for key, value := range m {
-// 		copy[key] = value
-// 	}
-// 	return copy
-// }
+func CopyMapStringInt(m map[string]int64) map[string]int64 {
+	copy := make(map[string]int64)
+	for key, value := range m {
+		copy[key] = value
+	}
+	return copy
+}
 
 /*
 CopyMap takes a map of any format and returns its copy. 
