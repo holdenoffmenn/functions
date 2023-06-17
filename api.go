3f74ebba9466dabb6232024b1250966296be1e88
Holden Offmenn <holden.luz@grupoicts.org.br>
2023-06-17 11:13:07 -0400

    Add

--- api.go
+++ api.go
@@ -11,4 +11,6 @@ type Client interface {
 	// CopyMapStringInt(m map[string]int64) map[string]int64
 
 	CopyMap(original interface{}) interface{}
+
+	CompareMapsStrIFace(map1, map2 map[string]interface{}) bool
 }
