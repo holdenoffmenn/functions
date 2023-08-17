fcd644523e9fe6e0e332d58114fceab746ee5e92
Holden Offmenn <holden.luz@grupoicts.org.br>
2023-08-17 17:15:57 -0400

    Add functions in api.go

--- api.go
+++ api.go
@@ -2,15 +2,17 @@ package functions
 
 type Client interface {
 
-	//MapsCheck(map1, map2 map[string]string) bool
+	CompareMapsStrIFace(map1, map2 map[string]interface{})
+
+	CompareMapsStrBool(map1, map2 map[string]bool) bool
+
+	CompareMapsStrUint32(map1, map2 map[string]uint32) bool
 
 	CompareMapsIFaceIFace(map1, map2 interface{}) bool
 
-	// CopyMapStringString(m map[string]string) map[string]string
+	CopyMapStringUint32(m map[string]uint32) map[string]uint32
 
-	// CopyMapStringInt(m map[string]int64) map[string]int64
+	CopyMapStringBool(m map[string]bool) map[string]bool
 
 	CopyMap(original interface{}) interface{}
-
-	CompareMapsStrIFace(map1, map2 map[string]interface{}) bool
 }
